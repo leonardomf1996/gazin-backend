1eb008844c2cf896c547db3a2b38405f96857831
Leonardo Furtado <leonardomf1996@gmail.com>
2021-04-28 18:55:16 -0300

    Repository

---
+++ src/repositories/DevelopersRepository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Developer } from '../models/Developer';
+
+@EntityRepository(Developer)
+class DevelopersRepository extends Repository<Developer> {
+
+}
+
+export { DevelopersRepository };
