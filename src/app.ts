c18e7c8210d84f04e64bb445d03dfd1578dd12d1
Leonardo Furtado <leonardomf1996@gmail.com>
2021-04-28 18:21:36 -0300

    AppError

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { AppError } from './errors/AppError';
 import cors from 'cors';
 
 const app = express();
@@ -8,5 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+   if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+         message: err.message
+      });
+   }
+
+   return response.status(500).json({
+      status: 'Error',
+      message: `Internal Server Error: ${err.message}`
+   });
+});
 
 export { app };
