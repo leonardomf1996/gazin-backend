d29af9aa1366c5eac2763e8285c05dec452a1fba
Leonardo Furtado <leonardomf1996@gmail.com>
2021-04-28 19:31:44 -0300

    Linha faltante no app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
    if (err instanceof AppError) {
